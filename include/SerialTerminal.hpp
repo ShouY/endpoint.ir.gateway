14a8e468dfbfd76276a0814288fbd0c05545ea83
Nan Wen <1532134362@qq.com>
2022-08-09 00:46:39 +0800

    first commit

---
+++ include/SerialTerminal.hpp
@@ -0,0 +1,203 @@
+#pragma once
+
+#define ST_VERSION "1.1.2"
+
+#include <Arduino.h>
+
+namespace maschinendeck {
+
+#if not defined ST_FLAG_NOBUILTIN && defined E2END
+#include "EEPROM.h"
+void printEEPROM(String opts) {
+  io_stream->print("offset \t");
+  for (uint8_t h = 0; h < 16; h++) {
+    io_stream->print(h, HEX);
+    io_stream->print('\t');
+  }
+  io_stream->print("\r\n");
+  for (uint8_t i = 0; i < (E2END / 16); i++) {
+    String line = "";
+    io_stream->print(i * 16, HEX);
+    io_stream->print('\t');
+    for (uint8_t n = 0; n <= 15; n++) {
+      size_t value = EEPROM.read(i * 16 + n);
+      io_stream->print(value, HEX);
+      io_stream->print('\t');
+      line += static_cast<char>(value);
+    }
+    io_stream->print(line);
+    io_stream->print("\r\n");
+  }
+}
+#endif
+
+struct Command {
+  String command;
+  void (*callback)(String param);
+  String description;
+
+  Command(String command, void (*callback)(String param), String description)
+      : command(command), callback(callback), description(description) {}
+};
+
+template <typename T, typename U>
+class Pair {
+ private:
+  T first_;
+  U second_;
+
+ public:
+  Pair(T first, U second) : first_(first), second_(second) {}
+  T first() { return this->first_; }
+
+  U second() { return this->second_; }
+};
+
+class SerialTerminal {
+ private:
+  Stream* io_stream;
+  Command* commands[256];
+  uint8_t size_;
+  bool firstRun;
+  String message;
+
+#ifndef ST_FLAG_NOHELP
+  void printCommands() {
+    for (uint8_t i = 0; i < this->size_; i++) {
+      io_stream->println("\t" + this->commands[i]->command + "\t" +
+                         this->commands[i]->description);
+    }
+#ifndef ST_FLAG_NOPROMPT
+    io_stream->print("st> ");
+#endif
+  }
+#endif
+
+ public:
+  SerialTerminal(Stream* io = nullptr)
+      : io_stream(io), size_(0), firstRun(true), message("") {
+#if not defined ST_FLAG_NOBUILTIN && defined E2END
+    this->add("eeprom", &printEEPROM, "prints the contents of EEPROM");
+#endif
+
+#ifndef ST_FLAG_NOHELP
+    if (io_stream) {
+      io_stream->print("SerialTerm v" ST_VERSION);
+      io_stream->print("\r\n");
+      io_stream->println("(C) 2022, MikO - Hpsaturn");
+      io_stream->println("  available commands:");
+    }
+#endif
+  }
+
+  void init(Stream* io) { io_stream = io; }
+
+  bool is_open() const { return io_stream && io_stream->availableForWrite(); }
+
+  void add(String command, void (*callback)(String param),
+           String description = "") {
+    if (this->size_ >= 64) return;
+    this->commands[this->size_] = new Command(command, callback, description);
+    this->size_++;
+  }
+
+  uint8_t size() { return this->size_ + 1; }
+
+  void loop() {
+    if (io_stream == nullptr) {
+      return;
+    }
+#ifndef ST_FLAG_NOHELP
+    if (this->firstRun) {
+      this->firstRun = false;
+      this->printCommands();
+    }
+#endif
+    if (!io_stream->available()) return;
+    bool commandComplete = false;
+    while (io_stream->available()) {
+      char car = io_stream->read();
+      if (car == 127 && this->message.length() > 0) {
+        io_stream->print("\e[1D");
+        io_stream->print(' ');
+        io_stream->print("\e[1D");
+        this->message.remove(this->message.length() - 1);
+        continue;
+      }
+      if (isAscii(car)) io_stream->print(car);
+      // Check if user ended the line
+      if (car == '\r') {
+        io_stream->print("\r\n");
+        commandComplete = true;
+        // If there are more data on the line, drop a \n, if it is
+        // there. Some terminals may send both, giving
+        // an extra lineend, if we do not drop it.
+        if (io_stream->available() && io_stream->peek() == '\n') {
+          io_stream->read();
+        }
+        io_stream->flush();
+        io_stream->read();
+        break;
+      }
+      this->message += car;
+    }
+
+    if (!commandComplete) return;
+    if (this->message == "") {
+#ifndef ST_FLAG_NOPROMPT
+      io_stream->print("st> ");
+#endif
+
+      return;
+    }
+
+    Pair<String, String> command = SerialTerminal::ParseCommand(this->message);
+    this->message = "";
+    bool found = false;
+    for (uint8_t i = 0; i < this->size_; i++) {
+      if (this->commands[i]->command == command.first()) {
+        this->commands[i]->callback(command.second());
+        found = true;
+      }
+    }
+    if (!found) {
+      io_stream->print("\n" + command.first());
+      io_stream->println(": command not found");
+    }
+#ifndef ST_FLAG_NOPROMPT
+    io_stream->print("\r\nst> ");
+#endif
+  }
+
+  static Pair<String, String> ParseCommand(String message) {
+    String keyword = "";
+    for (auto& car : message) {
+      if (car == ' ') break;
+      keyword += car;
+    }
+    if (keyword != "") message.remove(0, keyword.length());
+    keyword.trim();
+    message.trim();
+
+    return Pair<String, String>(keyword, message);
+  }
+
+  static String ParseArgument(String message) {
+    String keyword = "";
+    for (auto& car : message) {
+      if (car == '"') break;
+      keyword += car;
+    }
+    if (keyword != "") message.remove(0, keyword.length());
+    message.trim();
+    int msg_len = message.length();
+    if (msg_len > 0) {
+      message.remove(0, 1);
+      message.remove(msg_len - 2);
+    }
+
+    return message;
+  }
+};
+
+}  // namespace maschinendeck
---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+
+/*********************************************************************
+ This sample file is part of the esp32-wifi-cli:
+ Copyright (c) 2022, @hpsaturn, Antonio Vanegas
+ https://hpsaturn.com, All rights reserved.
+ https://github.com/hpsaturn/esp32-wifi-cli
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, version 3.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+#include <Arduino.h>
+// #include <M5Atom.h>
+
+// #include <AsyncTCP.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+#include "SerialTerminal.hpp"
+
+maschinendeck::SerialTerminal Terminal;
+
+/*********************************************************************
+ * User defined commands. Example: suspend, blink, reboot, etc.
+ ********************************************************************/
+void blink(String opts) {
+  maschinendeck::Pair<String, String> operands =
+      maschinendeck::SerialTerminal::ParseCommand(opts);
+  int times = operands.first().toInt();
+  int miliseconds = operands.second().toInt();
+  for (int i = 0; i < times; i++) {
+    // M5.dis.fillpix(0xaaff00);  // set LED to green
+    digitalWrite(LED_BUILTIN, LED_BRIGHTNESS);
+    delay(miliseconds);
+    // M5.dis.fillpix(0xff0000);  // set LED to green
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(miliseconds);
+  }
+}
+
+void echo(String opts) {
+  String echo = maschinendeck::SerialTerminal::ParseArgument(opts);
+  Serial.println("\r\nmsg: " + echo);
+}
+
+void reboot(String opts) { ESP.restart(); }
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Terminal.add("echo", echo, "echo parameters");
+  Terminal.add("reboot", reboot, "reboot MCU");
+  Terminal.add("blink", blink, "blink");
+
+  Serial.begin(115200);
+  Terminal.init(&Serial);
+  while (!Terminal.is_open()) {
+    delay(100);
+  }
+}
+
+void loop() {
+  // Terminla loop
+  Terminal.loop();
+}
